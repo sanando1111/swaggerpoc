4331f1727ecea5e4f021acce36f6b1ffaac5d22b
Wolverine <Wolverine@DESKTOP-EM5FHMA>
2018-11-10 20:50:16 +0530

    Change in Swagger
    Change-Id: I7519bac9a2966cc8be51c47828cacab34291b01d

--- SeriesApp/src/main/java/com/series/config/SwaggerConfig.java
+++ SeriesApp/src/main/java/com/series/config/SwaggerConfig.java
@@ -1,10 +1,8 @@
 package com.series.config;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
+import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -12,6 +10,8 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableAutoConfiguration
+//@EnableSwagger2
+@ComponentScan
 public class SwaggerConfig {                                    
     @Bean
     public Docket api() { 
